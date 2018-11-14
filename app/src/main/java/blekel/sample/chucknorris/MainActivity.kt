4156e703ef46c484aaf5c6d63ab7bc673036cb9c
Vitaliy Levonyuk <blekel.drow@gmail.com>
2018-11-14 18:56:14 +0200

    project init

---
+++ app/src/main/java/blekel/sample/chucknorris/MainActivity.kt
@@ -0,0 +1,12 @@
+package blekel.sample.chucknorris
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
